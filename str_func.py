541be5f1d473575d9e9fc8b86a71bbf6675f606f
Egor Ahryapin <fleg666@mail.ru>
2024-03-17 20:53:23 +0300

    Добавил функцию и коммиты в str_funk.py

--- str_func.py
+++ str_func.py
@@ -1,5 +1,12 @@
 def function_upper():
+    """ДОКСТРИНГ ДЛЯ КОНФЛИКТА"""
     txt = str(input())
     x = txt.upper()
+    return x
+
+def function_title():
+    """делает заглавными первые буквы каждого слова в строке, поступившей на вход функции"""
+    txt = str(input())
+    x = txt.title()
     return x
 
